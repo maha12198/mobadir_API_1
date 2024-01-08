012380e8e21c8b08b6ca11d1ca1bb324f370062a
Maha Mahmoud <maha.mahmoud12198@gmail.com>
2024-01-08 09:57:31 +0200

    published and ssl is working

--- ClientApp/src/app/config/constants.ts
+++ ClientApp/src/app/config/constants.ts
@@ -1,6 +1,7 @@
 export class Constants
 {
-  public static api_url: string = 'http://mobaaderr-001-site1.btempurl.com/api';
+  //public static api_url: string = 'http://mobaaderr-001-site1.btempurl.com/api';
+  public static api_url: string = 'https://mobaaderr-001-site1.btempurl.com/api';
 
   //public static api_url: string = 'http://ahamdycs2012-001-site1.btempurl.com/api';
   
--- Controllers/RichEditorController.cs
+++ Controllers/RichEditorController.cs
@@ -14,62 +14,12 @@ namespace mobadir_API_1.Controllers
     [ApiController]
     public class RichEditorController : ControllerBase
     {
-        private readonly IWebHostEnvironment _webHostEnvironment;
+        //private readonly IWebHostEnvironment _webHostEnvironment;
+        //public RichEditorController(IWebHostEnvironment webHostEnvironment)
+        //{
+        //    _webHostEnvironment = webHostEnvironment;
+        //}
 
-        public RichEditorController(IWebHostEnvironment webHostEnvironment)
-        {
-            _webHostEnvironment = webHostEnvironment;
-        }
-
-        [HttpPost("ImageUpload")]
-        public async Task<Object> ImageUpload()
-        {
-            try
-            {
-                IFormFile file = HttpContext.Request.Form.Files[0];
-                string folderPath = "wwwroot/ArticleImages/";
-
-                var baseUrl = Path.Combine(_webHostEnvironment.ContentRootPath,  folderPath);
-                
-                int total;
-                try
-                {
-                    total = HttpContext.Request.Form.Files.Count;
-                }
-                catch (Exception ex)
-                {
-                    return await Task.FromResult(new { error = new { message = "Error Uploading file" } });
-                }
-
-                if (total == 0)
-                {
-                    return await Task.FromResult(new { error = new { message = "No file has sent" } });
-                }
-                if (!Directory.Exists(baseUrl)) 
-                {
-                    return await Task.FromResult(new { error = new { message = "Folder does not exist" } });
-                }
-                string fileName = file.FileName;
-                if(fileName =="")
-                {
-                    return await Task.FromResult(new { error = new { message = "Error Uploading file" } });
-                }
-                string newFileName = Guid.NewGuid().ToString()+Path.GetExtension(fileName);
-                string newPath = baseUrl + newFileName;
-                using (var stream = System.IO.File.OpenWrite(newPath))
-                {
-                    await file.CopyToAsync(stream);
-                }
-                string imageUrl = "https://localhost:7199/ArticleImages/" + newFileName;
-
-                return await Task.FromResult(new {url = imageUrl});
-            }
-            catch (Exception ex)
-            {
-                // Log the exception or handle it in an appropriate way
-                return BadRequest(new { Message = "Image upload failed", Error = ex.Message });
-            }
-        }
 
         [HttpPost("ImageUpload_1")]
         public async Task<Object> ImageUpload_1()
@@ -85,10 +35,11 @@ namespace mobadir_API_1.Controllers
 
                 FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpUrl);
 
-                request.Credentials = new NetworkCredential("ahamdycs2012-001", "Ahmed123#");
+                //request.Credentials = new NetworkCredential("ahamdycs2012-001", "Ahmed123#");
+                request.Credentials = new NetworkCredential("mobaaderr-001", "12345.Net");
 
                 request.Method = WebRequestMethods.Ftp.UploadFile;
-                
+
                 request.UsePassive = true;
 
                 var totalBytes = file.Length;
@@ -102,6 +53,7 @@ namespace mobadir_API_1.Controllers
 
                 FtpWebResponse response = (FtpWebResponse)request.GetResponse();
                 //string imageUrl = "http://ahamdycs2012-001-site1.btempurl.com/uploads//" + newFileName;
+                //string imageUrl = "http://mobaaderr-001-site1.btempurl.com/uploads//" + newFileName;
                 string imageUrl = "https://mobaaderr-001-site1.btempurl.com/uploads//" + newFileName;
 
                 // new : for the file type 
@@ -109,16 +61,71 @@ namespace mobadir_API_1.Controllers
                 string fileExtension = Path.GetExtension(file.FileName).ToLower();
 
                 return await Task.FromResult(new { url = imageUrl, extension = fileExtension });
-                
+
             }
             catch (WebException ex)
             {
                 String status = ((FtpWebResponse)ex.Response).StatusDescription;
-                
+
                 // Log the exception or handle it in an appropriate way
                 return BadRequest(new { Message = "Image upload failed", Error = ex.Message });
             }
         }
+
+
+
+
+        //[HttpPost("ImageUpload")]
+        //public async Task<Object> ImageUpload()
+        //{
+        //    try
+        //    {
+        //        IFormFile file = HttpContext.Request.Form.Files[0];
+        //        string folderPath = "wwwroot/ArticleImages/";
+
+        //        var baseUrl = Path.Combine(_webHostEnvironment.ContentRootPath,  folderPath);
+
+        //        int total;
+        //        try
+        //        {
+        //            total = HttpContext.Request.Form.Files.Count;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            return await Task.FromResult(new { error = new { message = "Error Uploading file" } });
+        //        }
+
+        //        if (total == 0)
+        //        {
+        //            return await Task.FromResult(new { error = new { message = "No file has sent" } });
+        //        }
+        //        if (!Directory.Exists(baseUrl)) 
+        //        {
+        //            return await Task.FromResult(new { error = new { message = "Folder does not exist" } });
+        //        }
+        //        string fileName = file.FileName;
+        //        if(fileName =="")
+        //        {
+        //            return await Task.FromResult(new { error = new { message = "Error Uploading file" } });
+        //        }
+        //        string newFileName = Guid.NewGuid().ToString()+Path.GetExtension(fileName);
+        //        string newPath = baseUrl + newFileName;
+        //        using (var stream = System.IO.File.OpenWrite(newPath))
+        //        {
+        //            await file.CopyToAsync(stream);
+        //        }
+        //        string imageUrl = "https://localhost:7199/ArticleImages/" + newFileName;
+
+        //        return await Task.FromResult(new {url = imageUrl});
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Log the exception or handle it in an appropriate way
+        //        return BadRequest(new { Message = "Image upload failed", Error = ex.Message });
+        //    }
+        //}
+
+
     }
 
 
